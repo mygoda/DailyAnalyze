e1ef57532ba5607d501779cf9260441cc73ec5bb
Mac <tracy1303300@gmail.com>
2015-03-09 14:20:50 +0800

    增加部署配置文件

--- daily/config/__init__.py
+++ daily/config/__init__.py
@@ -5,8 +5,15 @@ from __future__ import division, unicode_literals, print_function
 from os.path import join
 from ConfigParser import ConfigParser
 
+try:
+    from server import ConfigOverride
+except ImportError:
+    ConfigOverride = []
 
 config = ConfigParser()
 config.readfp(open(join(__path__[0], 'default.cfg')))
+
+for(section, option, value) in ConfigOverride:
+    config.set(section, option, value)
 
 
---
+++ daily/config/server.py
@@ -0,0 +1,7 @@
+
+ConfigOverride = (
+	('db','engine','django.db.backends.mysql'),
+	('db','username','root'),
+	('db','password','900303'),
+	('db','db_name','daily'),
+)
